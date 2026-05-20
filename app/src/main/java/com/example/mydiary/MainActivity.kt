f45ea26b94289e2eee3d044ca13f9e80e1646cf0
khw3991 <khw430613@@gmail.com>
2026-05-20 09:31:03 +0900

    가상 디바이스 세팅

--- app/src/main/java/com/example/mydiary/MainActivity.kt
+++ app/src/main/java/com/example/mydiary/MainActivity.kt
@@ -1,20 +1,31 @@
 package com.example.mydiary
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.style.LineHeightStyle
+import androidx.compose.ui.tooling.preview.Preview
 
-class MainActivity : AppCompatActivity() {
+// 1. AppCompatActivity 대신 ComponentActivity를 상속받아야 합니다.
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContent {
+            MessageCard(name = "Android")
         }
     }
 }
+
+@Composable
+fun MessageCard(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewMessageCard() {
+    MessageCard(name = "Android")
+}
+
